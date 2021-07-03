81d2fd654e154e523d77d6c2a893f5495b6441f0
jn-mic <jackotienokey@gmail.com>
2021-07-03 11:53:22 +0300

    creating past photos function that will on project app

--- photo/views.py
+++ photo/views.py
@@ -1,5 +1,5 @@
-from django.http.response import HttpResponse
-from django.shortcuts import render
+from django.http.response import Http404, HttpResponse,40
+from django.shortcuts import render, redirect
 from django.conf import settings
 from . import views
 from django.conf.urls.static import static
@@ -11,9 +11,19 @@ from datetime import dt
 def photo_of_day(request):
     date=dt.date.today()
 
-    render(request,'welcome.html')
+    render(request,'all-photos/today-photos.html',{'date':date,})
 
-def past_photo(request):
-    past= dt.date()
+# view function to present  photos from the past days
+def past_photo(request,past_date):
+    try:
+        # Convert data from string url
+        date= dt.datetime.strptime(past_date,'%Y-%m-%d').date()
+    except ValueError:
+        #Raise 404 error when valueError is thrown
+        raise Http404()
+        assert False
+    if date == dt.date.today():
+        return redirect(photo_of_day)
+
 
     render(request,'past-photo.html')
