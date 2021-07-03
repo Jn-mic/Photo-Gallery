87fd2ce31c163b773e19757c5edae438dc1ea4e2
jn-mic <jackotienokey@gmail.com>
2021-07-03 16:36:18 +0300

    creating all test cases in module classes

--- photo/models.py
+++ photo/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+
 # from django.db.models.deletion import CASCADE
 
 # Create your models here.
@@ -9,8 +10,15 @@ class Editor(models.Model):
 
     def __str__(self):
         return self.first_name
-    class meta:
-        ordering=['first_name']
+    
+    def save_editor(self):
+        self.save()
+    
+    def delete_editor(self):
+        self.save()
+
+    def update_editor(self):
+        self.save()
 
 # creating a tag models in our database
 class tag(models.Model):
@@ -18,14 +26,38 @@ class tag(models.Model):
 
     def __str__(self):
         return self.name
+    def save_tag(self):
+        self.save()
+    
+    def delete_tag(self):
+        self.save()
+
+    def update_tag(self):
+        self.save()
 
 # creating a Article models in our database
 class Article(models.Model):
     title=models.CharField(max_length=30)
     post=models.TextField()
-    editor=models.ForeignKey(Editor)
+    editor=models.ForeignKey(Editor,on_delete=models.CASCADE)
     tags=models.ManyToManyField(tag)
     pub_date=models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.post
+
+    def save_Article(self):
+        self.save()
+    
+    def delete_Article(self):
+        self.save()
+
+    def update_Article(self):
+        self.save()
+
+
+
+
+
+
+    
--- photo/tests.py
+++ photo/tests.py
@@ -5,8 +5,78 @@ from .models import Editor, tag, Article
 class EditorTestClass(TestCase):
     #set up method
     def setUp(self):
-        self.moringa= Editor(first_name='moringa',last_name='student',email='ms@moringa.com')
+        self.Moringa= Editor(first_name='Moringa',last_name='student',email='ms@moringa.ke')
 
     # Testing Instance
     def test_instance(self):
-        self.assertTrue(isinstance(self.moringa, Editor))
+        self.assertTrue(isinstance(self.Moringa, Editor))
+
+    # Testing save method
+
+    def test_save_method(self):
+        self.Moringa.save_editor()
+        editors= Editor.objects.all()
+        self.assertTrue(len(editors)>0)
+
+    # Testing delete method
+    def test_delete_method(self):
+        self.Moringa.delete_editor()
+        editors= Editor.objects.all()
+        self.assertTrue(len(editors)>0)
+
+    # Testing update method
+    def test_update_method(self):
+        self.Moringa.update_editor()
+        editors= Editor.objects.all()
+        self.assertTrue(len(editors)>0)
+
+class tagTestClass(TestCase):
+    def setUp(self):
+        self.name=tag(tag_name='tech')
+    # Testing Instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.name, tag))
+
+    def test_save_method(self):
+        self.name.save_tag()
+        tags= tag.objects.all()
+        self.assertTrue(len(tags)>0)
+    
+    def test_delete_method(self):
+        self.name.save_tag()
+        tags= tag.objects.all()
+        self.assertTrue(len(tags)>0)
+
+    def test_update_method(self):
+        self.name.save_tag()
+        tags= tag.objects.all()
+        self.assertTrue(len(tags)>0)
+
+# Article Test.
+class ArticleTestClass(TestCase):
+    #set up method
+    def setUp(self):
+        self.Technology= Article(title='DATA-SCIENCE',Post='Review',editor='Jimmy')
+
+    # Testing Instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.Technology, Article))
+
+    # Testing save method
+
+    def test_save_method(self):
+        self.Technology.save_article()
+        article= Article.objects.all()
+        self.assertTrue(len(article)>0)
+
+    # Testing delete method
+    def test_delete_method(self):
+        self.Technology.delete_article()
+        article= Article.objects.all()
+        self.assertTrue(len(article)>0)
+
+    # Testing update method
+    def test_update_method(self):
+        self.Technology.update_article()
+        article= Editor.objects.all()
+        self.assertTrue(len(article)>0)
