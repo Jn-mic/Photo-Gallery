f40124dc6aa6c112a4428ed53e1c776c1be998e4
jn-mic <jackotienokey@gmail.com>
2021-07-03 10:58:49 +0300

    creating local time zone to Nairobi-Kenya on project settings

--- Photo_Gallaery/settings.py
+++ Photo_Gallaery/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Installed app
     'photo',
 ]
 
@@ -106,7 +108,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Nairobi-Kenya'
 
 USE_I18N = True
 
