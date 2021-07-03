2406dee3d2e0b9e20bff26017fbd7fe55d0ce025
jn-mic <jackotienokey@gmail.com>
2021-07-03 10:55:30 +0300

    installing app in our photo-gallary project

--- photo/urls.py
+++ photo/urls.py
@@ -1,4 +1,11 @@
-from django.urls import path
+from django.urls import path,include
+from django.urls.resolvers import URLPattern
 
 
 from . import views
+
+
+URLPattern=[
+    path('photo/',views.photo_of_day, name='photoToday'),
+
+]
--- photo/views.py
+++ photo/views.py
@@ -1,3 +1,14 @@
+from django.http.response import HttpResponse
 from django.shortcuts import render
+from django.conf import settings
+from . import views
+from django.conf.urls.static import static
+from django.http import HttpResponse
+from datetime import dt
+
 
 # Create your views here.
+def photo_of_day(request):
+    date=dt.date.today()
+
+    render('Welcome to my Photo Gallaery')
