4689b46198327157b132e8b37e707ff7a469d4d8
jn-mic <jackotienokey@gmail.com>
2021-07-03 15:05:05 +0300

    creating test instances for Editor class models

--- photo/models.py
+++ photo/models.py
@@ -1,7 +1,31 @@
 from django.db import models
+# from django.db.models.deletion import CASCADE
 
 # Create your models here.
 class Editor(models.Model):
     first_name=models.CharField(max_length=40)
     last_name=models.CharField(max_length=30)
     email=models.EmailField()
+
+    def __str__(self):
+        return self.first_name
+    class meta:
+        ordering=['first_name']
+
+# creating a tag models in our database
+class tag(models.Model):
+    name=models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.name
+
+# creating a Article models in our database
+class Article(models.Model):
+    title=models.CharField(max_length=30)
+    post=models.TextField()
+    editor=models.ForeignKey(Editor)
+    tags=models.ManyToManyField(tag)
+    pub_date=models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.post
--- photo/tests.py
+++ photo/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
+from .models import Editor, tag, Article
 
 # Create your tests here.
+class EditorTestClass(TestCase):
+    #set up method
+    def setUp(self):
+        self.moringa= Editor(first_name='moringa',last_name='student',email='ms@moringa.com')
+
+    # Testing Instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.moringa, Editor))
